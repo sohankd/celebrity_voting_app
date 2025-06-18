6cd50fe71183ec4dec8f78da64b9fc78da6908f4
Sohan <sohankd5419@gmail.com>
2025-06-19 02:21:14 +0530

    Added clear instructions to construct server url for making api requests and socket polling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+// Direct invocation to localhost or continer URI doesn't work when you're running on docker lab.
+// Instead use the lab SSH URL with exposed server port in `http://<SSH_STRING_BEFORE_@>-<SERVER_PORT>.<SSH_STRING_AFTER_@>
+// If Docker Lab SSH is ip172-18-0-28-d19gllq91nsg0084ri60@direct.labs.play-with-docker.com AND Exposed Server Port is 4000
+// Result: http://ip172-18-0-28-d19gllq91nsg0084ri60-4000.direct.labs.play-with-docker.com
+const SERVER_URL = 'http://ip172-18-0-28-d19gllq91nsg0084ri60-4000.direct.labs.play-with-docker.com';
+const socket = io(SERVER_URL);
 
 function App() {
 	const [celebrities, setCelebrities] = useState([]);
@@ -19,12 +24,12 @@ function App() {
 	}, []);
 
 	const fetchCelebs = async () => {
-		const res = await axios.get('http://localhost:3000/api/celebrities');
+		const res = await axios.get(`${SERVER_URL}/api/celebrities`);
 		setCelebrities(res.data);
 	};
 
 	const vote = async (id) => {
-		await axios.post(`http://localhost:3000/api/vote/${id}`);
+		await axios.post(`${SERVER_URL}/api/vote/${id}`);
 		// No need to manually refresh, server will emit update
 	};
 
