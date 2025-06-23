0e670fab3d1e7836e0d37e3ca463328e921fc0b9
Sohan <sohankd5419@gmail.com>
2025-06-23 20:14:35 +0530

    Shell scripts & Dockerfiles were updated to run efficiently on Docker lab environment

--- docker_lab_backend_app.sh
+++ docker_lab_backend_app.sh
@@ -1,12 +1,15 @@
-# STEPS TO AUTOMATE THE CONTENARIZATION OF CELEBRITY VOTING APP.
-
 # --- BACKEND APPLICATION ---
 
 # Step-1: Build an image for the backend server application
 docker build -f backend/Dockerfile -t sohan/server backend/
 
 # Step-2: Build a container from the server application image
-docker container create -p 4000:4000 --network server_db --name voting-server sohan/server
+docker container create -p 4000:4000 \
+    -e DB_SEED_SOURCE_URL="https://6b2d9484-51c3-4489-ad78-26ea2851231f.mock.pstmn.io/api/celebrities" \
+    -e SERVER_PORT=4000 \
+    -e MONGO_DB_URI="mongodb://mongodb:27017/voteApp" \
+    --network server_db --restart=always \
+    --name voting-server sohan/server
 
 # Step-3: Create a custom network conection for server application & client application
 docker network create client_server
@@ -16,17 +19,3 @@ docker network connect client_server voting-server
 
 # Step-5: Run voting-server container
 docker start voting-server
-
-# --- FRONTEND APPLICATION ---
-
-# Step-6: Build an image for the fronten client application
-docker build -f frontend/Dockerfile -t sohan/client frontend/
-
-# Step-7: Build a container from the client application image
-docker container create -p 3000:80 --network client_server --name voting-app sohan/client
-
-# Step-8: Run voting-server container
-docker start voting-app
-
-# Step-9: Seed MongDB database (Only required once post deployment)
-echo "Seed DB by running command: curl '<SERVER_ENDPOINT>/seed'"
--- docker_lab_frontend_app.sh
+++ docker_lab_frontend_app.sh
@@ -1,18 +1,13 @@
-# STEPS TO AUTOMATE THE CONTENARIZATION OF CELEBRITY VOTING APP.
-
 # --- FRONTEND APPLICATION ---
 
 # Step-1: Set REACT_APP_SERVER_URL environment variable to communicate with server.
-read -p 'Enter Server URL for React App: ' REACT_APP_SERVER_URL
+read -p "Enter Server URL for React App: " REACT_APP_SERVER_URL
 
 # Step-2: Build an image for the fronten client application
-docker build -f frontend/Dockerfile -t sohan/client frontend/
+docker build --build-arg REACT_APP_SERVER_URL=$REACT_APP_SERVER_URL -f frontend/Dockerfile -t sohan/client frontend/
 
 # Step-3: Build a container from the client application image
-docker container create -e REACT_APP_SERVER_URL=$REACT_APP_SERVER_URL -p 3000:80 --network client_server --name voting-app sohan/client
+docker container create -p 3000:80 --network client_server --name voting-app sohan/client
 
 # Step-4: Run voting-server container
 docker start voting-app
-
-# Step-5: Seed MongDB database (Only required once post deployment)
-echo "Seed DB by running command: curl '<SERVER_ENDPOINT>/seed'"
